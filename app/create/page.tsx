3bddb88309560e9dc0e628311e17eda2d6dc51a4
ashishbhintade <ashishbhintade@users.noreply.github.com>
2025-07-04 16:49:19 +0530

    build error changes

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,4 +1,10 @@
-import CreatePost from "@/components/CreatePost";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const CreatePost = dynamic(() => import("@/components/CreatePost"), {
+  ssr: false,
+});
 
 export default function Create() {
   return <CreatePost />;
